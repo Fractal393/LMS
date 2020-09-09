ab29ce0f256abc56f817776d2a375bb611ac0ee4
Fractal393 <54801158+Fractal393@users.noreply.github.com>
2020-09-09 19:54:15 +0530

    Update FileExplorer.js

--- client/src/components/views/FileExplorer/FileExplorer.js
+++ client/src/components/views/FileExplorer/FileExplorer.js
@@ -18,3 +18,32 @@ export default class App extends React.Component {
       </div>);
     }
 }
+ 
+                
+//AWS LINK
+/*
+import * as React from 'react';
+import { SampleBase } from '../common/sample-base';
+import { FileManagerComponent, Inject, NavigationPane, DetailsView, Toolbar, ContextMenu } from '@syncfusion/ej2-react-filemanager';
+// File Manager sample with Amazon S3 file provider service
+export class AmazonS3Provider extends SampleBase {
+    constructor() {
+        super(...arguments);
+        this.hostUrl = "https://amazons3.azurewebsites.net/api/AmazonS3Provider/";
+    }
+    render() {
+        return (<div>
+                <div className="control-section">
+                    <FileManagerComponent id="filemanager" ajaxSettings={{
+            url: this.hostUrl + 'AmazonS3FileOperations',
+            getImageUrl: this.hostUrl + 'AmazonS3GetImage',
+            uploadUrl: this.hostUrl + 'AmazonS3Upload',
+            downloadUrl: this.hostUrl + 'AmazonS3Download'
+        }} searchSettings={{ allowSearchOnTyping: false }}>
+                    <Inject services={[NavigationPane, DetailsView, Toolbar, ContextMenu]}/>
+                    </FileManagerComponent>
+                </div>
+            </div>);
+    }
+}
+*/               
